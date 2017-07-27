9da986a82ac3c7876e87cf8a36262dfea99ca8f8
Nasr Maswood <nmaswood@uchicago.edu>
2017-07-27 11:08:01 -0400

    foo

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
 import openpyxl as o
 import pandas as pd
 from glob import glob
-from calendar import monthrange, month_abbr
 from collections import namedtuple
+from datetime import datetime
+from dateutil.parser import parse
+import pandas as pd
 
-
-State = namedtuple('State', 'state list_of_months')
-Month = namedtuple('Month', 'month list_of_days')
+State = namedtuple('State', 'state start_year list_of_months')
+Month = namedtuple('Month', 'year month list_of_days')
 Day = namedtuple('Day', 'day percentage')
 
 FIRST_SHEET = 'colorado_2016_2017.xlsx'
@@ -82,6 +83,9 @@ def how_many_days_off_for_date(sheet, date_offset_var, how_many_schools):
     except IndexError:
         return None
 
+    if date.value is None:
+        return None
+
     school_day_dict = create_day_dict()
 
     for row_i in range(how_many_schools):
@@ -96,7 +100,7 @@ def how_many_days_for_month(sheet, num_schools):
     days = []
     for i in range(32):
 
-        val  = how_many_days_off_for_date(sheet, i, num_schools)
+        val = how_many_days_off_for_date(sheet, i, num_schools)
 
         if val is None:
             break
@@ -114,9 +118,11 @@ def process_sheet(sheet):
     num_schools = get_number_of_schools(sheet)
     day_percentage = how_many_days_for_month(sheet,num_schools)
     month = sheet.title.split()[0]
+    year = sheet.title.split()[1]
 
-    days = [Day(idx,value) for idx,value in enumerate(day_percentage)]
-    month_final = Month(month, days)
+
+    days = [Day(idx + 1,value) for idx,value in enumerate(day_percentage)]
+    month_final = Month(year, month, days)
 
     return month_final
 
@@ -130,12 +136,62 @@ def process_workbook(name):
 
     relevant_sheets = [sheet for sheet in wb.worksheets if '_' not in sheet.title and not sheet.title.startswith("School")]
 
-    state_name = name.split("_")[0].title()
+    splat = name.split("_")
+
+    state_name = splat[0].title()
+
+    start_year = splat[1]
 
     months = [process_sheet(sheet) for sheet in relevant_sheets]
 
-    return State(state_name, months)
+    return State(state_name,start_year, months)
+
+
+
+def str_date_format(month_tuple):
+
+    y = month_tuple.year
+    month = month_tuple.month
+
+    s =  month + " {}, 20" + y
+
+    return s
+
+
+def process_month(month_tuple):
+
+    base_str = str_date_format(month_tuple)
+    as_dates = [parse(base_str.format(day_obj.day)) for day_obj in month_tuple.list_of_days]
+    percentages = [day_obj.percentage for day_obj in month_tuple.list_of_days]
+
+    return as_dates, percentages
+
+def processed_workbook_to_dataframe(pwb):
+
+    state_name = pwb.state
+    list_of_months = pwb.list_of_months
+    start_year =  pwb.start_year
+
+    all_days = []
+    all_percentages = []
+
+    for month_tuple in list_of_months:
+        days, percentages = process_month(month_tuple)
+
+        all_days += days
+        all_percentages += percentages
+
+    d = {'date': all_days, state_name: all_percentages}
+
+    df = pd.DataFrame(d)
+    df = df.set_index('date')
+    return df
+
+def workbook_name_to_df(name):
+    results = process_workbook(FIRST_SHEET)
+    return processed_workbook_to_dataframe(results)
 
 if __name__ == '__main__':
-    results = process_workbook(FIRST_SHEET)
-    print (results)
+
+    res = workbook_name_to_df(FIRST_SHEET)
+    print (res.head())
